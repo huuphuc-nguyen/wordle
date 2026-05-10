b6a5803cd85fc1935c7685b1006c3191a09844df
Huu Phuc Nguyen <nhphuc236@gmail.com>
2026-05-10 00:46:52 -0500

    chore: change base name of router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wordle">
       <App />
     </BrowserRouter>
   </StrictMode>,
